3946737e70d10e0255b907146858e8f654bd714e
Sung M. Kim <sungmkim80@gmail.com>
2019-11-03 23:29:51 -0500

    able to find a file from json

--- src/index.js
+++ src/index.js
@@ -10,10 +10,14 @@ let rawData = JSON.parse(fs.readFileSync(path.join(__dirname, "articles.json")))
 // rawData.map(_ => _.feed_source_url).filter(Boolean)
 
 const pattern = /https:\/\/www.slightedgecoder.com\/(\d{4})\/(\d{2})\/(\d{2})\/(.+)\/$/gi
-const [_, m, d, y, slug] = [
+const [_, year, month, day, slug] = [
   ..."https://www.slightedgecoder.com/2018/12/18/page-not-found-on-netlify-with-react-router/".matchAll(
     pattern
   ),
 ][0]
 
-info(`m, d, y, slug`, m, d, y, slug)
+// info(`m, d, y, slug`, year, month, day, slug)
+
+const filename = path.join(__dirname, `../blog/${year}/${slug}/index.md`)
+const exists = fs.existsSync(filename)
+info(`${filename} exists????? ${exists}`)
