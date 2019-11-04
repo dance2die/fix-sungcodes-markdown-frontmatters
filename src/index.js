e5e386ac1fd58b993f29d62798a45056fde71f64
Sung M. Kim <sungmkim80@gmail.com>
2019-11-04 11:15:57 -0500

    into a loop

--- src/index.js
+++ src/index.js
@@ -9,32 +9,40 @@ const { info, warn, log, error } = console
 let rawData = JSON.parse(fs.readFileSync(path.join(__dirname, "articles.json")))
 // info(JSON.stringify(rawData.map(_ => _.feed_source_url), null, 2));
 // info(rawData.map(_ => _.feed_source_url).filter(Boolean));
-
 // rawData.map(_ => _.feed_source_url).filter(Boolean)
 
+// cached_tag_list: tags,
+// cached_user_name: author,
+
 const pattern = /https:\/\/www.slightedgecoder.com\/(\d{4})\/(\d{2})\/(\d{2})\/(.+)\/$/gi
-const [_, year, month, day, slug] = [
-  ..."https://www.slightedgecoder.com/2018/12/18/page-not-found-on-netlify-with-react-router/".matchAll(
-    pattern
-  ),
-][0]
-
-info(`year=${year}, month=${month}, day=${day}, slug=${slug}`)
-
-const filename = path.join(__dirname, `../blog/${year}/${slug}/index.md`)
-const exists = fs.existsSync(filename)
-// info(`${filename} exists????? ${exists}`)
-
-editor
-  .read(filename)
-  .data((data, matter) => {
-    matter.data = Object.assign(data, {
-      published_at: "2018-12-19T02:14:23.000Z",
-      date: `${year}-${month}-${day}`,
-    })
+rawData
+  .filter(_ => !!_.feed_source_url || !!_.canonical_url)
+  .forEach(post => {
+    const url = post.feed_source_url || post.canonical_url
+    const [_, year, month, day, slug] = [...url.matchAll(pattern)][0]
+    info(`year=${year}, month=${month}, day=${day}, slug=${slug}`)
   })
-  .save(path.join(__dirname, `../blog/${year}/${slug}/`), {}, (err, matter) => {
-    if (err) error(`ERRRORORORORORO!===> `, err)
-    console.log(matter)
-  })
-// .show("data")
+
+// const [_, year, month, day, slug] = [
+//   ..."https://www.slightedgecoder.com/2018/12/18/page-not-found-on-netlify-with-react-router/".matchAll(
+//     pattern
+//   ),
+// ][0]
+
+// const filename = path.join(__dirname, `../blog/${year}/${slug}/index.md`)
+// const exists = fs.existsSync(filename)
+// // info(`${filename} exists????? ${exists}`)
+
+// editor
+//   .read(filename)
+//   .data((data, matter) => {
+//     matter.data = Object.assign(data, {
+//       published_at: "2018-12-19T02:14:23.000Z",
+//       date: `${year}-${month}-${day}`,
+//     })
+//   })
+//   .save(path.join(__dirname, `../blog/${year}/${slug}/`), {}, (err, matter) => {
+//     if (err) error(`ERRRORORORORORO!===> `, err)
+//     console.log(matter)
+//   })
+// // .show("data")
